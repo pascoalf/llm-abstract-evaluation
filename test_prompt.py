9af5e6b1975485c4403eb4cc3dad39863ab37440
Francisco Pascoal <fpascoal1996@gmail.com>
2026-08-30 12:26:39 +0100

    Initial prompt setup

---
+++ test_prompt.py
@@ -0,0 +1,16 @@
+import create_pipeline as cp
+
+cp.main()
+
+# simple prompt
+test =  [{"role": "user", "content": "What is your purpose?"}]
+
+query1 = cp.pipe(test, do_sample=True, top_p=0.8, temperature = 0.1, clean_up_tokenization_spaces=False)
+
+#
+print("""Response to prompt below:
+|
+|
+|
+""")
+print(query1[0]["generated_text"])
