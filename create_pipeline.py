98947fbe34bd598ab489519ba08c37a5523363e1
Francisco Pascoal <fpascoal1996@gmail.com>
2026-08-30 12:06:55 +0100

    Prepare script to load open source LLM and define general settings

---
+++ create_pipeline.py
@@ -0,0 +1,35 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, GenerationConfig
+
+model = None
+pipe = None
+tokenizer = None
+
+def main():
+    global model, pipe, tokenizer
+    #
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
+
+    # Load model and tokenizer
+    model = AutoModelForCausalLM.from_pretrained(
+        "microsoft/Phi-3-mini-4k-instruct",
+        device_map = "auto",
+        torch_dtype = "auto",
+        attn_implementation="eager"
+    )
+    
+    generation_config = GenerationConfig(
+        max_new_tokens=100
+        )
+
+    # create a pipeline
+    pipe = pipeline(
+        "text-generation",
+        model = model,
+        tokenizer=tokenizer,
+        return_full_text = False,
+        generation_config = generation_config
+        )
+
+if __name__ == "__main__":
+    main()
