0af5309edbf8d53a510c9ac6d32dc9df4669682c
Francisco Pascoal <fpascoal1996@gmail.com>
2026-08-30 15:58:57 +0100

    Some tests to score a single abstract

---
+++ one_abstract.py
@@ -0,0 +1,52 @@
+import create_pipeline as cp
+from datasets import load_dataset
+
+cp.main()
+
+abstracts = load_dataset(
+    "slinusc/PubMedAbstractsSubset",
+    split="train",
+    streaming=True
+)
+
+some_abstracts = list(abstracts.take(20))
+
+# first example
+abstract = some_abstracts[1]["abstract"]
+
+# simple prompt
+eval_abstract1 = [
+    {
+        "role": "user",
+        "content": f"""
+                    Evaluate the clarity of presentation of the following scientific abstract.
+
+                    Rate its clarity from 0 to 5, where:
+                    0 = extremely unclear
+                    1 = very unclear
+                    2 = somewhat unclear
+                    3 = reasonably clear
+                    4 = very clear
+                    5 = exceptionally clear
+
+                    Return only the numerical score.
+
+                    Abstract:
+                    {abstract}
+                """
+    }
+]
+
+query1 = cp.pipe(eval_abstract1, 
+                 do_sample=True, 
+                 top_p=0.5, 
+                 temperature = 0.1, 
+                 clean_up_tokenization_spaces=False)
+
+#
+print("""Response to prompt below:
+|
+|
+|
+""")
+print(query1[0]["generated_text"])
--- test_prompt.py
+++ test_prompt.py
@@ -3,7 +3,8 @@ import create_pipeline as cp
 cp.main()
 
 # simple prompt
-test =  [{"role": "user", "content": "What is your purpose?"}]
+test =  [{"role": "user", 
+          "content": "What is your purpose?"}]
 
 query1 = cp.pipe(test, do_sample=True, top_p=0.8, temperature = 0.1, clean_up_tokenization_spaces=False)
 
