b4848948b6c90a5094f2a64c5aecab0fb038279e
Francisco Pascoal <fpascoal1996@gmail.com>
2026-09-01 17:42:35 +0100

    Slight timer improvement

--- compare_models.py
+++ compare_models.py
@@ -1,5 +1,6 @@
 import evaluate_model as em
 from datasets import load_dataset
+import time
 
 # 
 models = [
@@ -14,17 +15,27 @@ abstracts = load_dataset(
             streaming=True
         )
 
-some_abstracts = list(abstracts.take(2))
+some_abstracts = list(abstracts.take(20))
 
     # Extract titles used
 titles = [paper["title"] for paper in some_abstracts]
     # to store the results later
 all_results = []
 
+#
+start = time.perf_counter()
+
 # Run over all models, multiple seeds, all prompts
 for selected_model in models:
     em.evaluate_model(select_model = selected_model, 
-                      seed = 2, 
+                      seed = 5, 
                       some_abstracts=some_abstracts,
                       titles = titles,
-                      all_results = all_results)
+                      all_results = all_results,
+                      file_name= "all_models_5seed_20abstracts_3prompts")
+
+#
+
+elapsed = time.perf_counter() - start
+
+print(f"It took {elapsed} to run.")
